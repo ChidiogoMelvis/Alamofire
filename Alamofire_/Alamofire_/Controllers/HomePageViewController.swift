86e87541c335da5b15c60cbfa6f666ccf88cd239
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-07-11 04:06:53 +0100

    update on the ui

--- Alamofire_/Alamofire_/Controllers/HomePageViewController.swift
+++ Alamofire_/Alamofire_/Controllers/HomePageViewController.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
-class HomePageViewController: UIViewController {
+class HomePageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     var randomColors = ["blue", "yellow", "red"]
+    
+    var notificationButton = ReusableButton(image: "notification")
+    
+    var orientationButton = ReusableButton(image: "orientationLock")
+    
+    var settingsButton = ReusableButton(image: "Settings")
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [notificationButton, orientationButton, settingsButton])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 20
+        stack.axis = .horizontal
+        stack.distribution = .equalSpacing
+        return stack
+    }()
     
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -29,15 +44,19 @@ class HomePageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-        view.backgroundColor = .white
+//        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
     
     func setupViews() {
         view.addSubview(collectionView)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            
+            collectionView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
@@ -48,7 +67,7 @@ class HomePageViewController: UIViewController {
 
 
 
-extension HomePageViewController : UICollectionViewDataSource, UICollectionViewDelegate {
+extension HomePageViewController {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         randomColors.count
@@ -62,5 +81,9 @@ extension HomePageViewController : UICollectionViewDataSource, UICollectionViewD
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomePageCollectionViewCell", for: indexPath) as! HomePageCollectionViewCell
         return cell
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        <#code#>
+//    }
     
 }
---
+++ Alamofire_/Alamofire_/ReusableViews/Button.swift
@@ -0,0 +1,24 @@
+//
+//  Button.swift
+//  Alamofire_
+//
+//  Created by Mac on 11/07/2023.
+//
+
+import UIKit
+
+
+class ReusableButton: UIButton {
+
+//    override init(frame String: CGRect) {
+//    super.init(frame: frame)
+    init(image: String) {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    self.setImage(UIImage(named: image), for: .normal)
+}
+
+required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+}
+}
