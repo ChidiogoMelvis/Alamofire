ebcdf286ea91b46f188572370293b29f0fc203fb
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-07-14 14:21:23 +0100

    updated

---
+++ Alamofire_/Alamofire_/Controllers/SearchPages/TrackScreens/AlbumControlViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumControlViewController.swift
+//  Alamofire_
+//
+//  Created by Mac on 14/07/2023.
+//
+
+import UIKit
+
+class AlbumControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
